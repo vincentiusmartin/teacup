b435e89e7f3803e1e52248f2d98581ed5b32468b
Farica Zhuang <faricazjj@Faricas-MacBook-Pro.local>
2019-02-01 18:43:15 -0500

    Added average ROC curve for logistic regression

--- draft/DistanceLogReg.py
+++ draft/DistanceLogReg.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy import interp
 from sklearn.utils import shuffle
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import roc_curve,roc_auc_score, accuracy_score
@@ -49,22 +50,63 @@ def get_parameter(x, y):
     index = output.index(max(output))
     return K[index], output
 
-def train(k):
+def train(k, x ,y):
     clf = LogisticRegression(C=k)
     cv = KFold(n_splits=10,shuffle=False)
     # Compute ROC curve and ROC area
+    # i = 1
+    # fpr_list = []
+    # tpr_list = []
+    # print("Beginning our 10 fold cross validation")
+    # for train,test in cv.split(x,y):
+    #     y_score = clf.fit(x[train], y[train]).predict_proba(x[test])
+    #     y_score = y_score[:,0]
+    #     # Compute ROC curve
+    #     fpr, tpr, _ = roc_curve(y[test].ravel(), y_score.ravel())
+    #     fpr_list.append(fpr)
+    #     tpr_list.append(tpr)
+    #     i += 1
+
+    # Compute ROC curve and ROC area with averaging
+    tprs = []
+    aucs_val = []
+    base_fpr = np.linspace(0, 1, 101)
     i = 1
-    fpr_list = []
-    tpr_list = []
-    print("Beginning our 10 fold cross validation")
-    for train,test in cv.split(x,y):
-        y_score = clf.fit(x[train], y[train]).predict(x[test])
-        # Compute ROC curve
-        fpr, tpr, _ = roc_curve(y[test].ravel(), y_score.ravel())
-        fpr_list.append(fpr)
-        tpr_list.append(tpr)
+    for train, test in cv.split(x,y):
+        model = clf.fit(x[train], y[train])
+        y_score = model.predict_proba(x[test])
+        fpr, tpr, _ = roc_curve(y[test], y_score[:, 1])
+        auc = roc_auc_score(y[test], y_score[:,1])
+        plt.plot(fpr, tpr, 'b', alpha=0.15)
+        print("fold " + str(i) + " AUC: " + str(auc))
+        tpr = interp(base_fpr, fpr, tpr)
+        tpr[0] = 0.0
+        tprs.append(tpr)
+        aucs_val.append(auc)
         i += 1
-    return fpr_list, tpr_list
+
+    tprs = np.array(tprs)
+    mean_tprs = tprs.mean(axis=0)
+    std = tprs.std(axis=0)
+
+    tprs_upper = np.minimum(mean_tprs + std, 1)
+    tprs_lower = mean_tprs - std
+
+
+    plt.plot(base_fpr, mean_tprs, 'b', label = "Average AUC:" + str(sum(aucs_val)/len(aucs_val)))
+    plt.fill_between(base_fpr, tprs_lower, tprs_upper, color='grey', alpha=0.3)
+
+    plt.plot([0, 1], [0, 1],'r--')
+    plt.xlim([0, 1])
+    plt.ylim([0, 1])
+    plt.ylabel('True Positive Rate')
+    plt.xlabel('False Positive Rate')
+    plt.axes().set_aspect('equal', 'datalim')
+    plt.legend(loc="lower right")
+    plt.show()
+    plt.savefig('Avg_ROC.png')
+
+    #return fpr_list, tpr_list
 
 def display_output(fpr_list, tpr_list):
     for i in range(len(fpr_list)):
@@ -75,10 +117,11 @@ def display_output(fpr_list, tpr_list):
     plt.ylabel('True Positive Rate')
     plt.title('ROC Curves for All 10 Test Folds')
     plt.legend(loc="lower right")
-    plt.savefig('ROC.png')
+    plt.savefig('Avg_ROC.png')
 
 if __name__=="__main__":
     x,y = prepare_data(DATA_PATH)
     cv_parameter, acc_list = get_parameter(x,y)
-    fpr, tpr = train(cv_parameter)
-    display_output(fpr, tpr)
+    train(cv_parameter, x, y)
+    #fpr, tpr = train(cv_parameter, x, y)
+    #display_output(fpr, tpr)
