ba1a99515fc2946ec16f62a42b258b526eb1e994
Farica Zhuang <faricazjj@Faricas-MacBook-Pro.local>
2019-05-09 21:56:28 -0400

    updated compare.py

--- draft/compare.py
+++ draft/compare.py
@@ -109,13 +109,13 @@ def train_with_tune(x ,y):
     return aucs_val.mean(axis=0)
 
 
-def train(x ,y, n_est, max_depth):
-    #clf = LogisticRegression(C=k)
-    clf = RandomForestClassifier(n_estimators=n_est, max_depth=max_depth)
+def train(x ,y, clf):
+    
     cv = KFold(n_splits=10,shuffle=False)
 
     # Compute ROC curve and ROC area with averaging
     aucs_val = []
+    acc_val = []
     #--------------------get full roc----------------
     base_fpr = np.linspace(0, 1, 101)
     # --------------------get up to 0.2 fpr only--------------
@@ -125,72 +125,84 @@ def train(x ,y, n_est, max_depth):
     for train, test in cv.split(x,y):
         # train the model
         model = clf.fit(x[train], y[train])
-        y_score = model.predict_proba(x[test])[:,1]
+        #y_score = model.predict_proba(x[test])[:,1]
+        y_pred = model.predict(x[test])
         # get fpr and tpr
-        fpr, tpr, _ = roc_curve(y[test], y_score)
-        tpr = interp(base_fpr, fpr, tpr)
-        tpr[0] = 0.0
-        res_auc = auc(base_fpr, tpr)
-        aucs_val.append(res_auc)
+        # fpr, tpr, _ = roc_curve(y[test], y_score)
+        # tpr = interp(base_fpr, fpr, tpr)
+        # tpr[0] = 0.0
+        #res_auc = auc(base_fpr, tpr)
+        res_acc = accuracy_score(y_pred, y[test])
+        #aucs_val.append(res_auc)
+        acc_val.append(res_acc)
 
-    aucs_val = np.array(aucs_val)
-    return aucs_val.mean(axis=0)
+    #aucs_val = np.array(aucs_val)
+    acc_val = np.array(acc_val)
+    # return the average auc and accuracy from cross validation
+    #return aucs_val.mean(axis=0)
+    return acc_val.mean(axis=0)
 
 def display_output(output, names):
     # create boxplots for the lists in output
     plt.boxplot(output, labels=names)
     #plt.xticks(rotation=25)
-    plt.tight_layout()
-
+    #plt.tight_layout()
+    plt.xlabel("Combination index")
+    plt.ylabel("Accuracy")
     # Show the ROC curves for all 10 test folds on the same plot
-    plt.title('AUC Comparison Best Parameters')
+    plt.title('Accuracy Comparison Using Best Parameters')
     plt.gcf().subplots_adjust(top=0.85)
-    plt.savefig('Boxplot_feature_comb_best_param_auc.png')
+    plt.savefig('Boxplot_feature_comb_best_param_accuracy.png')
 
 if __name__=="__main__":
     # get data for all combinations
+    prog = 1
     output = []
     comb_name = []
+    prefix = ["distance", "1mer", "2mer", "3mer"]
     #--------------------------uncomment this if we're using best parameters----------------------
     # for auc's best param
-    comb_dict = {('distance',): [700,2], ('1mer',):[500,20], ('2mer',):[2000,20], ('3mer',):[700,20],\
-                 ('distance', '1mer'):[700,5], ('distance', '2mer'):[500,10],\
-                 ('distance', '3mer'):[500,15], ('1mer', '2mer'):[500,20], ('1mer', '3mer'):[1000,15],\
-                 ('2mer', '3mer'):[1000,15],('distance', '1mer', '2mer'):[500,10], \
-                 ('distance', '1mer', '3mer'):[1000,10], ('distance', '2mer', '3mer'):[2000,10],\
-                 ('1mer', '2mer', '3mer'):[1000,15], ('distance', '1mer', '2mer', '3mer'): [2000,10]}
-    # for acc best param
-    # comb_dict = {('distance',): [100,2], ('1mer',):[1000,15], ('2mer',):[2000,5], ('3mer',):[2000,20],\
-    #              ('distance', '1mer'):[1000,5], ('distance', '2mer'):[500,10],\
-    #              ('distance', '3mer'):[1000,15], ('1mer', '2mer'):[500,20], ('1mer', '3mer'):[2000,20],\
-    #              ('2mer', '3mer'):[2000,15],('distance', '1mer', '2mer'):[2000,15], \
-    #              ('distance', '1mer', '3mer'):[2000,15], ('distance', '2mer', '3mer'):[500,20],\
-    #              ('1mer', '2mer', '3mer'):[500,15], ('distance', '1mer', '2mer', '3mer'): [2000,20]}
+    # comb_dict_auc = {('distance',): [700,2], ('1mer',):[500,20], ('2mer',):[2000,20], ('3mer',):[700,20],\
+    #              ('distance', '1mer'):[700,5], ('distance', '2mer'):[500,10],\
+    #              ('distance', '3mer'):[500,15], ('1mer', '2mer'):[500,20], ('1mer', '3mer'):[1000,15],\
+    #              ('2mer', '3mer'):[1000,15],('distance', '1mer', '2mer'):[500,10], \
+    #              ('distance', '1mer', '3mer'):[1000,10], ('distance', '2mer', '3mer'):[2000,10],\
+    #              ('1mer', '2mer', '3mer'):[1000,15], ('distance', '1mer', '2mer', '3mer'): [2000,10]}
 
-    prefix = ["distance", "1mer", "2mer", "3mer"]
+    # for acc best param
+    comb_dict_acc = {('distance',): [100,2], ('1mer',):[1000,15], ('2mer',):[2000,5], ('3mer',):[2000,20],\
+                 ('distance', '1mer'):[1000,5], ('distance', '2mer'):[500,10],\
+                 ('distance', '3mer'):[1000,15], ('1mer', '2mer'):[500,20], ('1mer', '3mer'):[2000,20],\
+                 ('2mer', '3mer'):[2000,15],('distance', '1mer', '2mer'):[2000,15], \
+                 ('distance', '1mer', '3mer'):[2000,15], ('distance', '2mer', '3mer'):[500,20],\
+                 ('1mer', '2mer', '3mer'):[500,15], ('distance', '1mer', '2mer', '3mer'): [2000,20]}
+
     #prefix = ["1mer"]
-    f = open("compare_cv_output_best_param_auc.txt", 'w')
+    f = open("compare_cv_output_best_param_accuracy.txt", 'w')
     # get every feature combination
     # the number inside range will be the number of prefix we have
-
     for i in range(len(prefix)):
         print("Number of prefixes we're working on right now: ", (i+1))
         for comb in itertools.combinations(prefix, i+1):
             f.write("Current combination is " + str(comb))
             x, y = prepare_data(DATA_PATH, comb)
             comb_name.append(comb)
-            # run 10 times
+            # run 100 times
             auc_list = []
             for j in range(100):
+                print(prog/(15))
+                prog += 1
                 #------------------if using best parameters------------------
-                n_est = comb_dict[comb][0]
-                max_depth = comb_dict[comb][1]
+                # n_est = comb_dict_acc[comb][0]
+                # max_depth = comb_dict_acc[comb][1]
 
-                #-----------------if n_est and max_depth are set---------------
-                # n_est = 100
-                # max_depth = 2
+                # #-----------------if n_est and max_depth are set---------------
+                n_est = 1000
+                max_depth = 10
 
-                auc_res = train(x, y, n_est, max_depth)
+                rf = RandomForestClassifier(n_estimators=n_est, max_depth=max_depth)
+
+                auc_res = train(x, y, rf)
                 auc_list.append(auc_res)
             #output.append(auc_res)
             output.append(auc_list)
